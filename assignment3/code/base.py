2696c43902290747cfeed0eea2c14178c305394b
Philip Bale <pbale95@gmail.com>
2018-03-30 16:24:00 +0800

    Work

--- assignment3/code/base.py
+++ assignment3/code/base.py
@@ -1,19 +1,3 @@
-
-# Network parameters found "optimal" in Assignment 1
-INPUT_LAYER = 6
-HIDDEN_LAYER1 = 100
-OUTPUT_LAYER = 1
-TRAINING_ITERATIONS = 1000
-OUTFILE = './NN_OUTPUT/BACKPROP_LOG.txt'
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 20 17:17:14 2017
-
-@author: JTay
-"""
-
 import pandas as pd
 import numpy as np
 
@@ -22,45 +6,27 @@ from sklearn.pipeline import Pipeline
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 
+from helpers import load_data, nn_layers, nn_reg, nn_iter
+
+
 out = './results/base/'
-np.random.seed(0)
+df1_x, df1_y, df2_x, df2_y = load_data()
 
+grid ={'NN__alpha': nn_reg,'NN__hidden_layer_sizes': nn_layers}
 
-df1 = pd.read_csv("./data/perm.csv")
-df1_Y_VAL = 'case_status'
-
-df2 = pd.read_csv('./data/housing.csv')
-df2_Y_VAL = 'price_bracket'
-
-
-df1_y = df1[df1_Y_VAL].copy().values
-df1_x = df1.drop(df1_Y_VAL, 1).copy().values
-
-df2_y = df2[df2_Y_VAL].copy().values
-df2_x = df2.drop(df2_Y_VAL, 1).copy().values
-
-
-
-df1_x = StandardScaler().fit_transform(df1_x)
-df2_x = StandardScaler().fit_transform(df2_x)
-
-#%% benchmarking for chart type 2
-
-grid ={'NN__alpha':[10**-x for x in range(1,5)],'NN__hidden_layer_sizes':[(100,), (1, 100), (50, 50)]}
-
-mlp = MLPClassifier(activation='relu',max_iter=TRAINING_ITERATIONS,early_stopping=True,random_state=5)
+mlp = MLPClassifier(activation='relu',max_iter=nn_iter,early_stopping=True,random_state=5)
 pipe = Pipeline([('NN',mlp)])
 gs = GridSearchCV(pipe,grid,verbose=10,cv=5)
 
 gs.fit(df1_x,df1_y)
 tmp = pd.DataFrame(gs.cv_results_)
-tmp.to_csv(out+'1.csv')
+tmp.to_csv(out+'perm.csv')
 
 
-mlp = MLPClassifier(activation='relu',max_iter=TRAINING_ITERATIONS,early_stopping=True,random_state=5)
+mlp = MLPClassifier(activation='relu',max_iter=nn_iter,early_stopping=True,random_state=5)
 pipe = Pipeline([('NN',mlp)])
 gs = GridSearchCV(pipe,grid,verbose=10,cv=5)
 
 gs.fit(df2_x,df2_y)
 tmp = pd.DataFrame(gs.cv_results_)
-tmp.to_csv(out+'2.csv')
+tmp.to_csv(out+'housing.csv')
---
+++ assignment3/code/helpers.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+from collections import Counter
+from sklearn.metrics import accuracy_score as acc
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.feature_selection import mutual_info_classif as MIC
+from sklearn.base import TransformerMixin,BaseEstimator
+import scipy.sparse as sps
+from scipy.linalg import pinv
+
+nn_layers = [(100,), (50,), (50, 50)]
+nn_reg = [10**-x for x in range(1,5)]
+nn_iter = 1500
+
+def load_data():
+  np.random.seed(0)
+
+
+  perm = pd.read_csv("./data/perm.csv")
+  perm_Y_VAL = 'case_status'
+
+  housing = pd.read_csv('./data/housing.csv')
+  housing_Y_VAL = 'price_bracket'
+
+
+  perm_y = perm[perm_Y_VAL].copy().values
+  perm_x = perm.drop(perm_Y_VAL, 1).copy().values
+
+  housing_y = housing[housing_Y_VAL].copy().values
+  housing_x = housing.drop(housing_Y_VAL, 1).copy().values
+
+
+  perm_x = StandardScaler().fit_transform(perm_x)
+  housing_x = StandardScaler().fit_transform(housing_x)
+
+  return (perm_x, perm_y, housing_x, housing_y)
+
+
+def cluster_acc(Y,clusterLabels):
+    assert (Y.shape == clusterLabels.shape)
+    pred = np.empty_like(Y)
+    for label in set(clusterLabels):
+        mask = clusterLabels == label
+        sub = Y[mask]
+        target = Counter(sub).most_common(1)[0][0]
+        pred[mask] = target
+#    assert max(pred) == max(Y)
+#    assert min(pred) == min(Y)    
+    return acc(Y,pred)
+
+
+class myGMM(GMM):
+    def transform(self,X):
+        return self.predict_proba(X)
