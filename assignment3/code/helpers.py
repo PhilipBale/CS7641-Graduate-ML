459d3d7abbe051989f011c8315a77f555d6ec41c
Philip Bale <pbale95@gmail.com>
2018-03-30 17:56:15 +0800

    work on randomized forest

--- assignment3/code/helpers.py
+++ assignment3/code/helpers.py
@@ -135,3 +135,13 @@ def reconstructionError(projections,X):
     reconstructed = ((p@W)@(X.T)).T # Unproject projected data
     errors = np.square(X-reconstructed)
     return np.nanmean(errors)
+    
+class ImportanceSelect(BaseEstimator, TransformerMixin):
+    def __init__(self, model, n=1):
+         self.model = model
+         self.n = n
+    def fit(self, *args, **kwargs):
+         self.model.fit(*args, **kwargs)
+         return self
+    def transform(self, X):
+         return X[:,self.model.feature_importances_.argsort()[::-1][:self.n]]
