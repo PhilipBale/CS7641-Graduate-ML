499052e8765db550eda50c6b538d05f16f1b6b92
Philip Bale <pbale95@gmail.com>
2018-04-01 23:00:11 +0800

    Work

--- assignment3/code/pca.py
+++ assignment3/code/pca.py
@@ -13,51 +13,51 @@ import pandas as pd
 out = './results/pca/'
 
 perm_x, perm_y, housing_x, housing_y = load_data() # perm, housing
+raise Exception('wtf')
+
+
+# 2
+
+pca = PCA(random_state=5)
+pca.fit(perm_x)
+tmp = pd.Series(data = pca.explained_variance_,index = range(1,8))
+tmp1 = pd.Series(data = pca.explained_variance_ratio_,index = range(1,8))
+tmp2 = pd.Series(data = pca.explained_variance_ratio_.cumsum(),index = range(1,8))
+tmp3 = pd.Series(data = pca.singular_values_,index = range(1,8))
+pd.concat([tmp, tmp1, tmp2, tmp3], axis=1).to_csv(out+'perm scree.csv')
+
+
+pca = PCA(random_state=5)
+pca.fit(housing_x)
+tmp = pd.Series(data = pca.explained_variance_,index = range(1,13))
+tmp1 = pd.Series(data = pca.explained_variance_ratio_,index = range(1,13))
+tmp2 = pd.Series(data = pca.explained_variance_ratio_.cumsum(),index = range(1,13))
+tmp3 = pd.Series(data = pca.singular_values_,index = range(1,13))
+pd.concat([tmp, tmp1, tmp2, tmp3], axis=1).to_csv(out+'housing scree.csv')
+
 # raise Exception('wtf')
+#4
+
+grid ={'pca__n_components':dims,'NN__alpha':nn_reg,'NN__hidden_layer_sizes':nn_layers}
+pca = PCA(random_state=5)       
+mlp = MLPClassifier(activation='relu',max_iter=nn_iter,early_stopping=True,random_state=5)
+pipe = Pipeline([('pca',pca),('NN',mlp)])
+gs = GridSearchCV(pipe,grid,verbose=10,cv=5)
+
+gs.fit(perm_x,perm_y)
+tmp = pd.DataFrame(gs.cv_results_)
+tmp.to_csv(out+'perm dim red.csv')
 
 
-# # 2
+grid ={'pca__n_components':dims,'NN__alpha':nn_reg,'NN__hidden_layer_sizes':nn_layers}
+pca = PCA(random_state=5)       
+mlp = MLPClassifier(activation='relu',max_iter=nn_iter,early_stopping=True,random_state=5)
+pipe = Pipeline([('pca',pca),('NN',mlp)])
+gs = GridSearchCV(pipe,grid,verbose=10,cv=5)
 
-# pca = PCA(random_state=5)
-# pca.fit(perm_x)
-# tmp = pd.Series(data = pca.explained_variance_,index = range(1,8))
-# tmp1 = pd.Series(data = pca.explained_variance_ratio_,index = range(1,8))
-# tmp2 = pd.Series(data = pca.explained_variance_ratio_.cumsum(),index = range(1,8))
-# tmp3 = pd.Series(data = pca.singular_values_,index = range(1,8))
-# pd.concat([tmp, tmp1, tmp2, tmp3], axis=1).to_csv(out+'perm scree.csv')
-
-
-# pca = PCA(random_state=5)
-# pca.fit(housing_x)
-# tmp = pd.Series(data = pca.explained_variance_,index = range(1,13))
-# tmp1 = pd.Series(data = pca.explained_variance_ratio_,index = range(1,13))
-# tmp2 = pd.Series(data = pca.explained_variance_ratio_.cumsum(),index = range(1,13))
-# tmp3 = pd.Series(data = pca.singular_values_,index = range(1,13))
-# pd.concat([tmp, tmp1, tmp2, tmp3], axis=1).to_csv(out+'housing scree.csv')
-
-# # raise Exception('wtf')
-# #4
-
-# grid ={'pca__n_components':dims,'NN__alpha':nn_reg,'NN__hidden_layer_sizes':nn_layers}
-# pca = PCA(random_state=5)       
-# mlp = MLPClassifier(activation='relu',max_iter=nn_iter,early_stopping=True,random_state=5)
-# pipe = Pipeline([('pca',pca),('NN',mlp)])
-# gs = GridSearchCV(pipe,grid,verbose=10,cv=5)
-
-# gs.fit(perm_x,perm_y)
-# tmp = pd.DataFrame(gs.cv_results_)
-# tmp.to_csv(out+'perm dim red.csv')
-
-
-# grid ={'pca__n_components':dims,'NN__alpha':nn_reg,'NN__hidden_layer_sizes':nn_layers}
-# pca = PCA(random_state=5)       
-# mlp = MLPClassifier(activation='relu',max_iter=nn_iter,early_stopping=True,random_state=5)
-# pipe = Pipeline([('pca',pca),('NN',mlp)])
-# gs = GridSearchCV(pipe,grid,verbose=10,cv=5)
-
-# gs.fit(housing_x,housing_y)
-# tmp = pd.DataFrame(gs.cv_results_)
-# tmp.to_csv(out+'housing dim red.csv')
+gs.fit(housing_x,housing_y)
+tmp = pd.DataFrame(gs.cv_results_)
+tmp.to_csv(out+'housing dim red.csv')
 
 # 3
 dim = 5
--- assignment3/code/plotter.py
+++ assignment3/code/plotter.py
@@ -5,6 +5,13 @@ from helpers import clusters
 
 colors = ['r', 'b', 'g', 'k', 'm', 'c', 'y']
 
+all_folders = ['clustering', 'ica', 'pca', 'random_forest', 'randomized_projections']
+all_folders_name = ['Original', 'ICA', 'PCA', 'Random Forest', 'Randomized Projections']
+
+nn_items = ['', 'param_ica__n_components', 'param_pca__n_components', 'param_filter__n', 'param_rp__n_components'] 
+
+nn_cols = ['mean_fit_time', 'mean_test_score', 'mean_train_score', 'param_NN__alpha', 'param_NN__hidden_layer_sizes']
+
 def plot_learning_curve(iterations, train_scores, test_scores, title):
 #     _, _, test_scores_base = base_curve
 
@@ -253,9 +260,6 @@ def plot_feature_importances(folderFile, title):
     save_plot(plt, title)
     plt.close()
 
-all_folders = ['clustering', 'ica', 'pca', 'random_forest', 'randomized_projections']
-all_folders_name = ['Original', 'ICA', 'PCA', 'Random Forest', 'Randomized Projections']
-
 def plot_all_sse(valName, title):
     title = title + ' SSE Comparison after DR'
     plt.figure()
@@ -357,6 +361,73 @@ def plot_all_scoring(fileName, title, kMeans=True):
     save_plot(plt, title)
     plt.close()
 
+
+def plot_nn(fileBase, extra, xLabel, title):
+    title = title + ' Scoring'
+    plt.figure()
+    plt.title(title) 
+
+    plt.xlabel(xLabel)
+    plt.ylabel('Score')
+    
+    plt.grid()
+
+    index = 0
+    for folder in all_folders:
+        fileName = fileBase + extra
+
+        if index == 0:
+            folder = 'base'
+            fileName = fileBase
+            index += 1
+            continue
+
+        line = '*-'
+        if index == 0:
+            line = 'o-'
+
+        color = colors[index]
+
+        name = all_folders_name[index] 
+
+        df = get_df(folder + '/' + fileName + '.csv').sort_values('rank_test_score')
+        best = df.head(1)
+        # print(best)
+
+        best_data = best[nn_cols]
+
+        best_data = map(lambda x: str(x), list(best_data.values[0, :]))
+        # print(best_data)
+        print(name + ' & ' + ' & '.join(best_data) + ' \\\\ \\hline')
+
+        xVar = nn_items[index]
+
+        nnAlpha = list(best['param_NN__alpha'].values)[0]
+        nnLayers = list(best['param_NN__hidden_layer_sizes'].values)[0]
+
+        dfLayers = df['param_NN__hidden_layer_sizes'] == nnLayers
+        dfAlpha = df['param_NN__alpha'] == nnAlpha 
+
+        best_series = df[dfLayers & dfAlpha].sort_values(xVar)
+        # print(best_series)
+
+        xVals = list(set(df[xVar].values))
+
+        vals = best_series['mean_test_score'].values
+
+        # print(xVals)
+        # print(vals)
+
+        plt.plot(xVals, vals, line, color=color,
+             label=name + ' NN')
+
+        index += 1
+
+    plt.legend(loc="best")
+
+    save_plot(plt, title)
+    plt.close()
+
 def plot_clustering():
     plot_sse('clustering', 'perm SSE (left)', 'Perm Visa SSE')
     plot_sse('clustering', 'housing SSE (left)', 'Housing SSE')
@@ -441,6 +512,8 @@ def plot_comparison():
 
     plot_all_scoring('perm', 'Perm Visa k-Means after DR', True)
     plot_all_scoring('perm', 'Perm Visa Estimation Maximization after DR', False)
+
+    plot_nn('housing', ' dim red', '# Components', 'Housing NN after DR') 
 
 
 # plot_clustering()
